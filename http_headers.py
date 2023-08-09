f80d1e310bf8ca046e57ed2c40b3c70b91d6fd27
Abhinand Krishna AM <99167984+abhinandkrishna20@users.noreply.github.com>
2023-08-09 12:41:55 +0530

    modified all things

--- http_headers.py
+++ http_headers.py
@@ -1,26 +1,33 @@
-import requests
+import requests as reqs
 
-url = "https://www.sample.com/"  # Replace with the desired URL
 
 # Send an HTTP GET request to the specified URL
-response = requests.get(url)
 
-# Print request headers
-print("Request Headers:")
-print("User-Agent:", response.request.headers.get('User-Agent'))
-print("Host:", response.request.headers.get('Host'))
-print("Accept:", response.request.headers.get('Accept'))
-print("Authorization:", response.request.headers.get('Authorization'))
-print("Cookie:", response.request.headers.get('Cookie'))
-print("Referer:", response.request.headers.get('Referer'))
-print("Cache-Control:", response.request.headers.get('Cache-Control'))
+def req(response):
+    # response = requests.get(url)
+    # Print request headers
+    print("\n\nRequest Headers:")
+    print("\n\nUser-Agent:", response.request.headers.get('User-Agent'))
+    print("\n\nHost:", response.request.headers.get('Host'))
+    print("\n\nAccept:", response.request.headers.get('Accept'))
+    print("\n\nAuthorization:", response.request.headers.get('Authorization'))
+    print("\n\nCookie:", response.request.headers.get('Cookie'))
+    print("\n\nReferer:", response.request.headers.get('Referer'))
+    print("\n\nCache-Control:", response.request.headers.get('Cache-Control'))
 
-# Print response headers
-print("\nResponse Headers:")
-print("Server:", response.headers.get('Server'))
-print("Content-Type:", response.headers.get('Content-Type'))
-print("Content-Length:", response.headers.get('Content-Length'))
-print("Location:", response.headers.get('Location'))
-print("Set-Cookie:", response.headers.get('Set-Cookie'))
-print("ETag:", response.headers.get('ETag'))
-print("Cache-Control:", response.headers.get('Cache-Control'))
+def res(response):
+    # Print response headers
+    print("\n\nResponse Headers:")
+    print("\n\nServer:", response.headers.get('Server'))
+    print("\n\nContent-Type:", response.headers.get('Content-Type'))
+    print("\n\nContent-Length:", response.headers.get('Content-Length'))
+    print("\n\nLocation:", response.headers.get('Location'))
+    print("\n\nSet-Cookie:", response.headers.get('Set-Cookie'))
+    print("\n\nETag:", response.headers.get('ETag'))
+    print("\n\nCache-Control:", response.headers.get('Cache-Control'))
+
+if __name__ == "__main__":
+    url = input("\n\tPlease Enter the url:- ")  # Replace with the desired URL
+    response = reqs.get(url)
+    req(response)
+    res(response)
